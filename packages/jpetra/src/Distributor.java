bbbcbfdbd1c214a402b594e331d7c7a5ad823b73
Jason Cross <jcross>
2003-07-07 17:09:12 +0000

    More fixes for the addition of the Distributor.

--- packages/jpetra/src/Distributor.java
+++ packages/jpetra/src/Distributor.java
@@ -1,9 +1,12 @@
 package Jpetra;
 
+import java.io.Serializable;
+
 public interface Distributor {
+    
     public DistObject CreateFromSends(int NumExportIDs, int[] ExportPIDs, boolean Deterministic); //, int NumRemoteIDs
 
     public DistObject CreateFromReceives(int NumRemoteIDs, int RemoteGIDs, int[] RemotePIDs, boolean Deterministic); //int NumExportIDs, int[] ExportGIDs, int[] ExportPIDs
 
-    public Serilizable Do (Serilizable[] exportObjects);
+    public Serializable[] Do (Serializable[] exportObjects);
 }
--- packages/jpetra/src/SerialComm.java
+++ packages/jpetra/src/SerialComm.java
@@ -169,4 +169,8 @@ public class SerialComm extends JpetraObject implements Comm {
         return 1;
     }
     
+    public Distributor createDistributor() {
+    	return new SerialDistributor(this);
+    }
+    
 }
