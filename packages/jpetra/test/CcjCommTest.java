477eda1acc1399b210eb63d2f63cdbc8ebcba0a8
Jason Cross <jcross>
2004-05-18 14:28:03 +0000

    Used tpetra as a model for many of the classes.

--- packages/jpetra/test/CcjCommTest.java
+++ packages/jpetra/test/CcjCommTest.java
@@ -30,21 +30,22 @@ import Jpetra.*;
 
 public class CcjCommTest extends JpetraObject {
     public CcjCommTest(String[] args) {
+        this.initializeOutput();
         if(args.length > 0 && args[0].equals("-v")) setRootPrint("VERBOSE", true);
-    
+        
         int size = 1;
         int rank = 0;
-    
+        
         Jpetra.CcjComm comm = new Jpetra.CcjComm("ccjhosts.txt");
         
         int myPid = comm.getVnodeID();
         int numProc = comm.getNumVnodes();
         println("VERBOSE", "Processor "+myPid+" of "+numProc);
-    
-        System.exit(0); 
+        
+        System.exit(0);
     }
     
-    public static void main (String[] args) {
-        new CcjCommTest(args);  
+    public static void main(String[] args) {
+        new CcjCommTest(args);
     }
 }
