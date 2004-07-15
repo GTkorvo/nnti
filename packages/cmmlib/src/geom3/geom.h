ccd36899616aab12b444f95ddad12d44de0c14e7
Mark P. Sears <mpsears@sandia.gov>
2004-07-15 16:40:20 +0000

    minor formatting edit.

--- packages/cmmlib/src/geom3/geom.h
+++ packages/cmmlib/src/geom3/geom.h
@@ -14,12 +14,12 @@ extern "C"
   void axpy3(r8 a, r8 * x, r8 * y);                 // y = y + a*x
   void scale3(r8 s, r8 * x, r8 * y);                // y = s*x
   void drot3(r8 c, r8 * x, r8 s, r8 * y, r8 * z);   // ?
-  r8 dist3(r8 * x, r8 * y);                         // return |x-y|
-  r8 dot3(r8 * x, r8 * y);                          // return x.y
+  r8   dist3(r8 * x, r8 * y);                         // return |x-y|
+  r8   dot3(r8 * x, r8 * y);                          // return x.y
   void cross3(r8 * x, r8 * y, r8 * z);              // z = x cross y
-  r8 norm3(r8 * x);                                 // return |x|
-  r8 normalize3(r8 * x);                            // return |x| and normalize x.
-  r8 angle3(r8 * a, r8 * b);                        // return angle between two vectors
+  r8   norm3(r8 * x);                                 // return |x|
+  r8   normalize3(r8 * x);                            // return |x| and normalize x.
+  r8   angle3(r8 * a, r8 * b);                        // return angle between two vectors
 
 #ifdef __cplusplus
 }
