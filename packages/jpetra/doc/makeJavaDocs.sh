5a3abb1dd4cc5bca1fd91a707063688744546f7e
Michael N. Phenow <mnphenow>
2005-05-02 18:42:04 +0000

    Changed the output directory for the javadocs so as to avoid a collision with the doxygen output.

--- packages/jpetra/doc/makeJavaDocs.sh
+++ packages/jpetra/doc/makeJavaDocs.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 cd ../src
-javadoc -d ../doc/html -linksource -author -private Jpetra Jpetra.MatrixMarketIO Jpetra.CcjSupport
+javadoc -d ../doc/html-javadoc -linksource -author -private Jpetra Jpetra.MatrixMarketIO Jpetra.CcjSupport
