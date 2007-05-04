9f041709025760e3fa0480a9020d65c338e6d52d
Christopher G. Baker <bakercg@ornl.gov>
2007-05-04 18:00:50 +0000

    compiler warning indicated a statement   var == true; that should have been   var = true; i fixed it.
    also, removed tabs.

--- packages/rbgen/src/RBGen_MSPreprocessor.cpp
+++ packages/rbgen/src/RBGen_MSPreprocessor.cpp
@@ -35,7 +35,7 @@ namespace RBGen {
     std::vector<std::string>::iterator p = preproc_types_.begin();
     while ( p != preproc_types_.end() && !typeValid ) {
       if ( *p == preprocType_ )
-	typeValid == true;
+        typeValid = true;
       ++p;
     }
     // if (!typeValid) THROW AN ERROR!!!
@@ -45,10 +45,10 @@ namespace RBGen {
       // Try to get the input file from the parameter list
       //
       try {
-	input_file_ = Teuchos::getParameter< std::string >( preproc_params, "Filename" );
+        input_file_ = Teuchos::getParameter< std::string >( preproc_params, "Filename" );
       }
       catch (std::exception &e) {
-	cout<<"The modified snapshot input file has not been specified"<<endl;
+        cout<<"The modified snapshot input file has not been specified"<<endl;
       }
     }      
     //
@@ -60,20 +60,20 @@ namespace RBGen {
     }
     
     /*    try {
-	  scalings_ = Teuchos::getParameter< std::vector< double > >( *params, "Snapshot Scaling" );
-	  }
-	  catch (std::exception &e) {
-	  cout<<"The snapshot scaling vector has not been specified in the input!"<<endl;
-	  }
-	  //
-	  // Try to get the snapshot scaling indices vector
-	  //
-	  try {
-	  scaling_idx_ = Teuchos::getParameter< std::vector< std::pair<int,int> > >( *params, "Snapshot Scaling Indices" );
-	  }
-	  catch (std::exception &e) {
-	  cout<<"The snapshot scaling indices have not been specified in the input!"<<endl;
-	  }      
+      scalings_ = Teuchos::getParameter< std::vector< double > >( *params, "Snapshot Scaling" );
+      }
+      catch (std::exception &e) {
+      cout<<"The snapshot scaling vector has not been specified in the input!"<<endl;
+      }
+      //
+      // Try to get the snapshot scaling indices vector
+      //
+      try {
+      scaling_idx_ = Teuchos::getParameter< std::vector< std::pair<int,int> > >( *params, "Snapshot Scaling Indices" );
+      }
+      catch (std::exception &e) {
+      cout<<"The snapshot scaling indices have not been specified in the input!"<<endl;
+    }      
     */
     if ( scalings_.size() != scaling_idx_.size() ) {
       cout<<"The scaling vector is not the same size as the number of index pairs!"<< endl;
@@ -89,44 +89,44 @@ namespace RBGen {
     if (isInitialized_) {      
   
       if (preprocType_ == "Input File") {
-	//
-	// Get the steady state vector from the file.
-	//
-	std::vector< std::string > steady_file( 1, input_file_ );
-	msVector_ = fileio_->Read( steady_file );
-	Epetra_MultiVector* colMV = 0;
-	// 
-	// Remove the scaled steady state vector.
-	//
-	for (int i=0; i<ss->NumVectors(); i++) {
-	  colMV = new Epetra_MultiVector( View, *ss, i, 1 );
-	  colMV->Update( -1.0*scale_, *msVector_, 1.0 );
-	  delete colMV;
-	}
+        //
+        // Get the steady state vector from the file.
+        //
+        std::vector< std::string > steady_file( 1, input_file_ );
+        msVector_ = fileio_->Read( steady_file );
+        Epetra_MultiVector* colMV = 0;
+        // 
+        // Remove the scaled steady state vector.
+        //
+        for (int i=0; i<ss->NumVectors(); i++) {
+          colMV = new Epetra_MultiVector( View, *ss, i, 1 );
+          colMV->Update( -1.0*scale_, *msVector_, 1.0 );
+          delete colMV;
+        }
       }
       else if (preprocType_ == "Arithmetic Mean") {
-	//
-	// Compute the arithmetic mean from the input snapshots
-	//
-	msVector_ = Teuchos::rcp( new Epetra_MultiVector( ss->Map(), 1 ) );
-	Epetra_MultiVector* colMV = 0;
-	// 
-	// Sum up all the multivectors; compute the average.
-	//
-	for (int i=0; i<ss->NumVectors(); i++) {
-	  colMV = new Epetra_MultiVector( View, *ss, i, 1 );
-	  msVector_->Update( 1.0, *colMV, 1.0 );
-	  delete colMV;
-	}
-	msVector_->Scale( 1.0 / ss->NumVectors() );
-	// 
-	// Remove the arithmetic mean from the snapshot set.
-	//
-	for (int i=0; i<ss->NumVectors(); i++) {
-	  colMV = new Epetra_MultiVector( View, *ss, i, 1 );
-	  colMV->Update( -1.0*scale_, *msVector_, 1.0 );
-	  delete colMV;
-	}      
+        //
+        // Compute the arithmetic mean from the input snapshots
+        //
+        msVector_ = Teuchos::rcp( new Epetra_MultiVector( ss->Map(), 1 ) );
+        Epetra_MultiVector* colMV = 0;
+        // 
+        // Sum up all the multivectors; compute the average.
+        //
+        for (int i=0; i<ss->NumVectors(); i++) {
+          colMV = new Epetra_MultiVector( View, *ss, i, 1 );
+          msVector_->Update( 1.0, *colMV, 1.0 );
+          delete colMV;
+        }
+        msVector_->Scale( 1.0 / ss->NumVectors() );
+        // 
+        // Remove the arithmetic mean from the snapshot set.
+        //
+        for (int i=0; i<ss->NumVectors(); i++) {
+          colMV = new Epetra_MultiVector( View, *ss, i, 1 );
+          colMV->Update( -1.0*scale_, *msVector_, 1.0 );
+          delete colMV;
+        }      
       }
     }
     else {
