GTkorvo/nnti

commit 1df04658d178c4aeba0979f9751ee58fbc48e458

MueLu: add a new "MaxNodesPerAggregate" parameter in UncoupledAggregation
The new parameter gives the user direct control over the maximum possible
size of an aggregate in the UncoupledAggregationFactory.
The default value is set to 100 such that it should not change the old
behaviour where we had not such a parameter.

packages/muelu/src/Graph/UncoupledAggregation/MueLu_MaxLinkAggregationAlgorithm_def.hpp
@@ -74,9 +74,12 @@ namespace MueLu {
   BuildAggregates(const ParameterList& params, const GraphBase& graph, Aggregates& aggregates, std::vector<unsigned>& aggStat, LO& numNonAggregatedNodes) const {
     Monitor m(*this, "BuildAggregates");
 
+    LO MaxNodesPerAggregate          = params.get<LO>                  ("MaxNodesPerAggregate");
+
     // vertex ids for output
     ArrayRCP<LO> vertex2AggId = aggregates.GetVertex2AggId()->getDataNonConst(0);
     ArrayRCP<LO> procWinner   = aggregates.GetProcWinner()  ->getDataNonConst(0);
+    ArrayRCP<LO> aggSizes     = aggregates.ComputeAggregateSizes(); // contains number of nodes in aggregate with given aggId
 
     const LO  nRows  = graph.GetNodeNumVertices();
     const int myRank = graph.GetComm()->getRank();
@@ -120,7 +123,8 @@ namespace MueLu {
       for (size_t i = 0; i < aggSize; i++) {
         curNumConnections++;
         if (aggList[i+1] != aggList[i]) {
-          if (curNumConnections > maxNumConnections) {
+          if (curNumConnections > maxNumConnections &&         // only select aggregate if it has more connections
+              aggSizes[aggList[i]] < MaxNodesPerAggregate) {   // and if it is not too big (i.e. can have one more node)
             maxNumConnections = curNumConnections;
             selectedAggregate = aggList[i];
           }

packages/muelu/src/Graph/UncoupledAggregation/MueLu_UncoupledAggregationAlgorithm_def.hpp
@@ -78,6 +78,9 @@ namespace MueLu {
     AggOptions::Ordering ordering    = params.get<AggOptions::Ordering>("Ordering");
     LO MaxNeighAlreadySelected       = params.get<LO>                  ("MaxNeighAlreadySelected");
     LO MinNodesPerAggregate          = params.get<LO>                  ("MinNodesPerAggregate");
+    LO MaxNodesPerAggregate          = params.get<LO>                  ("MaxNodesPerAggregate");
+
+    TEUCHOS_TEST_FOR_EXCEPTION(MaxNodesPerAggregate < MinNodesPerAggregate, Exceptions::RuntimeError, "MueLu::UncoupledAggregationAlgorithm::BuildAggregates: MinNodesPerAggregate must be smaller or equal to MaxNodePerAggregate!");
 
     if (ordering != NATURAL && ordering != RANDOM && ordering != GRAPH)
       throw Exceptions::RuntimeError("UncoupledAggregation::BuildAggregates : bad aggregation ordering option");
@@ -155,8 +158,14 @@ namespace MueLu {
 
             if (aggStat[neigh] == NodeStats::READY || aggStat[neigh] == NodeStats::NOTSEL) {
               // Add neighbor node to tentative aggregate
-              aggList[aggSize++] = neigh;
-
+              // but only if aggregate size is not exceeding maximum size
+              // NOTE: We do not exit the loop over all neighbours since we have still
+              //       to count all aggregated neighbour nodes for the aggregation criteria
+              // NOTE: We check here for the maximum aggregation size. If we would do it below
+              //       with all the other check too big aggregates would not be accepted at all.
+              if(aggSize < MaxNodesPerAggregate) {
+                aggList[aggSize++] = neigh;
+              }
             } else {
               numAggregatedNeighbours++;
             }

packages/muelu/src/Graph/UncoupledAggregation/MueLu_UncoupledAggregationFactory_def.hpp
@@ -100,6 +100,7 @@ namespace MueLu {
                                   "If a new aggregate has some neighbours that are already aggregated, "
                                   "this node probably can be added to one of these aggregates. We don't need a new one.");
     validParamList->set<LO>      ("MinNodesPerAggregate",       2, "Minimum number of nodes for aggregate");
+    validParamList->set<LO>      ("MaxNodesPerAggregate",       100, "Maximum number of nodes for aggregate");
 
     validParamList->set<bool> ("UseOnePtAggregationAlgorithm",             false, "Allow special nodes to be marked for one-to-one transfer to the coarsest level. (default = off)");
     validParamList->set<bool> ("UseSmallAggregatesAggregationAlgorithm",   false, "Turn on/off build process for small aggregates in user defined regions. (default = off)");

commit 496bb9bee30b6bcecc40c0e03bdfdc76aec10fda

MueLu: fixing memory problem
This commit fixes a problem introduced by commit 1df04658 (added a new
"MaxNodesPerAggregate" parameter).  The issue is that the MaxLinkPhase
was computing aggregate sizes and then altering them.  This then showed
up as invalid memory reads in the CoordinatesTransferFactory, and
resulted in botched coarse grid coordinates.

The fix is to change the default behavior of ComputeAggregateSizes to
always recompute the aggregate sizes.  Thanks to Andrey for reviewing a
preliminary patch and suggesting improvements.

ComputeAggregateSizes now takes two boolean arguments.  The first
(default=true) determines whether to force recomputation.  The second
(default=false) determines whether the computed sizes are then cached.

I traced the issue using this test:

mpirun -np 15 valgrind ./MueLu_ScalingTestParamList.exe --nx=36 --ny=36
--nz=36 --matrixType=Laplace3D  --linAlgebra=Tpetra --xml=saZ1rcb.xml
--notimings --solver=none

and these settings in the input deck for the repartitioning.

    <ParameterList name="myRepartitionFact">
      <Parameter name="factory" type="string" value="RepartitionFactory"/>
      <Parameter name="A" type="string" value="myRAPFact"/>
      <Parameter name="Partition" type="string" value="myZoltanInterface"/>
      <Parameter name="minRowsPerProcessor"                 type="int" value="100"/>
      <Parameter name="nonzeroImbalance" type="double" value="1.1"/>
      <Parameter name="startLevel"                          type="int" value="1"/>
      <Parameter name="remapPartitions"                     type="bool" value="true"/>
    </ParameterList>

It's also important to set

   <Parameter name="maxCoarseSize"                         type="int" value="1"/>

packages/muelu/src/Graph/MueLu_Aggregates_decl.hpp
@@ -165,11 +165,19 @@ namespace MueLu {
     const RCP<const Map> GetMap() const; ///< returns (overlapping) map of aggregate/node distribution
 
     /*! @brief Compute sizes of aggregates
-     *
-     * returns the number of nodes in each aggregate in an array.
-     *
+      
+      Returns the number of nodes in each aggregate in an array.
+      If the aggregate sizes are not stored internally (which is the default), they are computed and returned.
+      If the aggregate sizes have been stored internally, then they are *not* recomputed, but instead the
+      stored sizes are returned.
+
+      @param[in] forceRecompute if true, force recomputation of the aggregate sizes.
+
+      @param[in] cacheSizes if true and either forceRecompute==true or sizes haven't yet been calculated, compute and store the sizes.
+      If forceRecompute==false and sizes have been calculated previously, then this parameter has no effect.
+      Sizes should only be cached when the aggregation phases are complete, i.e, aggregate sizes are no longer changing!
      */
-    Teuchos::ArrayRCP<LO> ComputeAggregateSizes() const;
+    Teuchos::ArrayRCP<LO> ComputeAggregateSizes(bool forceRecompute=true, bool cacheSizes=false) const;
 
     //! @name Overridden from Teuchos::Describable
     //@{

packages/muelu/src/Graph/MueLu_Aggregates_def.hpp
@@ -92,22 +92,35 @@ namespace MueLu {
 
   ///////////////////////////////////////////////////////
   template <class LocalOrdinal, class GlobalOrdinal, class Node, class LocalMatOps>
-  Teuchos::ArrayRCP<LocalOrdinal>  Aggregates<LocalOrdinal, GlobalOrdinal, Node, LocalMatOps>::ComputeAggregateSizes() const {
-    if (aggregateSizes_ == Teuchos::null)
-      {
-        aggregateSizes_ = Teuchos::ArrayRCP<LO>(nAggregates_,0);
-        int myPid = vertex2AggId_->getMap()->getComm()->getRank();
-        Teuchos::ArrayRCP<LO> procWinner   = procWinner_->getDataNonConst(0);
-        Teuchos::ArrayRCP<LO> vertex2AggId = vertex2AggId_->getDataNonConst(0);
-        LO size = procWinner.size();
+  Teuchos::ArrayRCP<LocalOrdinal>  Aggregates<LocalOrdinal, GlobalOrdinal, Node, LocalMatOps>::ComputeAggregateSizes(bool forceRecompute, bool cacheSizes) const {
 
-        //for (LO i = 0; i < nAggregates_; ++i) aggregateSizes_[i] = 0;
-        for (LO k = 0; k < size; ++k ) {
-          if (procWinner[k] == myPid) aggregateSizes_[vertex2AggId[k]]++;
-        }
+    if (aggregateSizes_ != Teuchos::null && !forceRecompute) {
+
+      return aggregateSizes_;
+
+    } else {
+
+      //invalidate previous sizes.
+      aggregateSizes_ = Teuchos::null;
+
+      Teuchos::ArrayRCP<LO> aggregateSizes;
+      aggregateSizes = Teuchos::ArrayRCP<LO>(nAggregates_,0);
+      int myPid = vertex2AggId_->getMap()->getComm()->getRank();
+      Teuchos::ArrayRCP<LO> procWinner   = procWinner_->getDataNonConst(0);
+      Teuchos::ArrayRCP<LO> vertex2AggId = vertex2AggId_->getDataNonConst(0);
+      LO size = procWinner.size();
+
+      //for (LO i = 0; i < nAggregates_; ++i) aggregateSizes[i] = 0;
+      for (LO k = 0; k < size; ++k ) {
+        if (procWinner[k] == myPid) aggregateSizes[vertex2AggId[k]]++;
       }
 
-    return aggregateSizes_;
+      if (cacheSizes)
+        aggregateSizes_ = aggregateSizes;
+
+      return aggregateSizes;
+    }
+
   } //ComputeAggSizesNodes
 
   template <class LocalOrdinal, class GlobalOrdinal, class Node, class LocalMatOps>

packages/muelu/src/Graph/UncoupledAggregation/MueLu_MaxLinkAggregationAlgorithm_def.hpp
@@ -88,6 +88,8 @@ namespace MueLu {
     const unsigned   magicConstAsDefaultSize = 100;
     std::vector<int> aggList(magicConstAsDefaultSize);
 
+    bool recomputeAggregateSizes=false;
+
     for (LO iNode = 0; iNode < nRows; iNode++) {
       if (aggStat[iNode] == NodeStats::AGGREGATED)
         continue;
@@ -127,6 +129,7 @@ namespace MueLu {
               aggSizes[aggList[i]] < MaxNodesPerAggregate) {   // and if it is not too big (i.e. can have one more node)
             maxNumConnections = curNumConnections;
             selectedAggregate = aggList[i];
+            recomputeAggregateSizes=true;
           }
           curNumConnections = 0;
         }

packages/muelu/src/Misc/MueLu_CoordinatesTransferFactory_def.hpp
@@ -131,7 +131,7 @@ namespace MueLu {
     // Get some info about aggregates
     int                         myPID        = uniqueMap->getComm()->getRank();
     LO                          numAggs      = aggregates->GetNumAggregates();
-    ArrayRCP<LO>                aggSizes     = aggregates->ComputeAggregateSizes();
+    ArrayRCP<LO>                aggSizes     = aggregates->ComputeAggregateSizes(true,true);
     const ArrayRCP<const LO>    vertex2AggID = aggregates->GetVertex2AggId()->getData(0);
     const ArrayRCP<const LO>    procWinner   = aggregates->GetProcWinner()->getData(0);
 