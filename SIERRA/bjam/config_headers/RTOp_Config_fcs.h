bf7f709b5a5f529e94e35351ff91f8ff34770a09
Greg Eisenhauer <eisen@cc.gatech.edu>
2014-12-15 09:25:55 -0500

    Baseline modifications to isolate NNTI from Trilinos.

--- SIERRA/bjam/config_headers/RTOp_Config_fcs.h
+++
@@ -1,7 +0,0 @@
-#define HAVE_MPI
-
-/* #undef HAVE_RTOP_DEBUG */
-
-/* #undef HAVE_RTOP_EXPLICIT_INSTANTIATION */
-
-#define RTOP_DEPRECATED
--- packages/jpetra/doc/makeJavaDocs.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-cd ../src
-javadoc -d ../doc/html-javadoc -linksource -author -private Jpetra Jpetra.MatrixMarketIO Jpetra.CcjSupport
